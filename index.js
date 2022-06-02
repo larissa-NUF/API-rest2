682894d73a29d7db9739b334fdc42712c0c06800
larissa-NUF <larissanuf.figueredo@gmail.com>
2022-06-01 23:57:01 -0300

    correução conecção

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const DB_USER = process.env.DB_USER
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
 
 mongoose
-    .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.jbqlt.mongodb.net/?retryWrites=true&w=majority`)
+    .connect(`mongodb://${DB_USER}:${DB_PASSWORD}@cluster0-shard-00-00.jbqlt.mongodb.net:27017,cluster0-shard-00-01.jbqlt.mongodb.net:27017,cluster0-shard-00-02.jbqlt.mongodb.net:27017/?ssl=true&replicaSet=atlas-cwskpq-shard-0&authSource=admin&retryWrites=true&w=majority`)
     .then(
         () => {
             console.log('Conectamos com o MongoDB!')
